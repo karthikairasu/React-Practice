7a623026e371dcce434fa5586d18717f875093fe
karthikairasu k <38223418+karthikairasu@users.noreply.github.com>
2026-01-03 18:14:16 +0000

    StopWatch

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import "./styles.css";
 import RealTime from "./components/TimeComponent";
 import StartStopCount from "./components/StartStopCountComponent";
+import StopWatchComponet from "./components/StopWatchComponent";
 
 export default function App() {
   return (
     <div className="App">
       <RealTime />
       <StartStopCount />
+      <StopWatchComponet />
     </div>
   );
 }
