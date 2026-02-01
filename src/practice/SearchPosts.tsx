7a9c623059ae7f518527f59eb1d79e5db02265ad
karthikairasu k <38223418+karthikairasu@users.noreply.github.com>
2026-02-01 18:28:34 +0000

    updating changes

--- src/practice/SearchPosts.tsx
+++ src/practice/SearchPosts.tsx
@@ -3,20 +3,43 @@ import axios from "axios";
 import { Table, Container } from "react-bootstrap";
 
 const SearchPosts = () => {
-  const [post, setPost] = useState([]);
+  const [posts, setPosts] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
-  const [hasError, setHasError] = useState(null);
+  const [error, setError] = useState(null);
+  const [searchInput, setSearchInput] = useState("");
+  const [debouncingSearch, setDebouncingSearch] = useState(searchInput);
 
+  const filterData = posts.filter((value, index) =>
+    value.toLowerCase().includes(searchInput.toLowerCase())
+  );
   useEffect(() => {
     const fetchData = async () => {
       const res = await axios.get("https://jsonplaceholder.typicode.com/posts");
-      setPost(res.data);
+      setPosts(res.data);
     };
     fetchData();
   }, []);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setDebouncingSearch(searchInput);
+      return () => clearTimeout(timer);
+    }, 500);
+  }, [searchInput]);
+
+  useEffect(() => {
+    if (debouncingSearch) {
+    }
+  }, [debouncingSearch]);
+
   return (
     <>
       <Container>
+        <input
+          type="text"
+          className="form-control my-2"
+          placeholder="Search Post"
+        />
         {isLoading && <h3>Loading...</h3>}
         <Table></Table>
       </Container>
