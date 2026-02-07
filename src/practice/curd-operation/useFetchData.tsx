cba7c8994b405482016164f823e14d890dfffd02
karthikairasu k <38223418+karthikairasu@users.noreply.github.com>
2026-02-07 18:06:41 +0000

    changes

--- src/practice/curd-operation/useFetchData.tsx
+++ src/practice/curd-operation/useFetchData.tsx
@@ -1,9 +1,19 @@
-import React, {useState} from "react";
+import React, { useState, useEffect } from "react";
 
 const useFetchData = () => {
   const [posts, setPosts] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
   const [error, setError] = useState(false);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try{
+        const res = fetch('');
+        const data = res.json();
+        setPosts(data);
+      }
+    };
+  }, []);
 };
 
 export default useFetchData;
